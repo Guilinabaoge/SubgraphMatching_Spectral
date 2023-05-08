1ed99645b75882d2059d23dba9925e8631d06316
Guilinabaoge <42078245+Guilinabaoge@users.noreply.github.com>
2023-05-08 18:02:19 +0200

    Experiment update

--- matching/main.cpp
+++ matching/main.cpp
@@ -171,46 +171,48 @@ void fixed_order_experiment(int argc, char** argv){
 int main(int argc, char** argv) {
 //    generate_datagraph_eigenvector("../../test/reallife_dataset/hprd/data_graph/hprd.graph","hprd.csv",35);
     Experiments::datagraphEigenMatrix = "youtube.csv";
-
     string datagraph = "../../test/reallife_dataset/youtube/data_graph/youtube.graph";
 
-
     string percentage[5] = {"0","0.25","0.5","0.75","1"};
-    for(auto p: percentage){
-        string querygraph = "../../test/wildcard/youtube/dense_32_1_"+ p+".graph";
-//        pair <matching_algo_outputs,matching_algo_outputs> LDF = MatchingWrapper(datagraph,querygraph,"LDF");
-//        pair <matching_algo_outputs,matching_algo_outputs> NLF = MatchingWrapper(datagraph,querygraph,"NLF");
-//        pair <matching_algo_outputs,matching_algo_outputs> GQL = MatchingWrapper(datagraph,querygraph,"GQL");
-        pair <matching_algo_outputs,matching_algo_outputs> TSOF = MatchingWrapper(datagraph,querygraph,"TSO");
-        return 0;
-        pair <matching_algo_outputs,matching_algo_outputs> CFL = MatchingWrapper(datagraph,querygraph,"CFL");
-        pair <matching_algo_outputs,matching_algo_outputs> DPiso = MatchingWrapper(datagraph,querygraph,"DPiso");
+    for(int i =5; i<21;i++){
+        for(auto p: percentage){
+            cout<<i<<endl;
+            string querygraph = "../../test/wildcard/youtube/dense_32_"+to_string(i)+"_"+ p+".graph";
+            pair <matching_algo_outputs,matching_algo_outputs> LDF = MatchingWrapper(datagraph,querygraph,"LDF");
+            pair <matching_algo_outputs,matching_algo_outputs> NLF = MatchingWrapper(datagraph,querygraph,"NLF");
+            pair <matching_algo_outputs,matching_algo_outputs> GQL = MatchingWrapper(datagraph,querygraph,"GQL");
+            pair <matching_algo_outputs,matching_algo_outputs> TSOF = MatchingWrapper(datagraph,querygraph,"TSO");
+            pair <matching_algo_outputs,matching_algo_outputs> CFL = MatchingWrapper(datagraph,querygraph,"CFL");
+            pair <matching_algo_outputs,matching_algo_outputs> DPiso = MatchingWrapper(datagraph,querygraph,"DPiso");
 //    matching_algo_outputs KF = Experiments::experiment3(datagraph,querygraph,"KF","0",NULL);
 
-        vector<pair<matching_algo_outputs,matching_algo_outputs>> evaluations;
-//        evaluations.push_back(LDF);
-//        evaluations.push_back(NLF);
-//        evaluations.push_back(GQL);
-        evaluations.push_back(TSOF);
-        evaluations.push_back(CFL);
-        evaluations.push_back(DPiso);
+            vector<pair<matching_algo_outputs,matching_algo_outputs>> evaluations;
+            evaluations.push_back(LDF);
+            evaluations.push_back(NLF);
+            evaluations.push_back(GQL);
+            evaluations.push_back(TSOF);
+            evaluations.push_back(CFL);
+            evaluations.push_back(DPiso);
 
-        std::ostringstream oss;
+            std::ostringstream oss;
+            oss<<"dense_32_"+to_string(i)+"_"+ p;
 
-        for(auto &eval : evaluations){
-            oss<<","<<eval.first.candidate_count_sum<<","<<eval.second.candidate_count_sum;
+            for(auto &eval : evaluations){
+                oss<<","<<eval.first.candidate_count_sum<<","<<eval.second.candidate_count_sum;
+            }
+
+            string file_path = "";
+            file_path = "pruning_power_experiment/wildcard/youtube.csv";
+
+            std::string var = oss.str();
+
+            std::ofstream myfile;
+            myfile.open (file_path,std::ios_base::app);
+            myfile<<var<<"\n";
+            myfile.close();
         }
-
-        string file_path = "";
-        file_path = "pruning_power_experiment/wildcard/youtube.csv";
-
-        std::string var = oss.str();
-
-        std::ofstream myfile;
-        myfile.open (file_path,std::ios_base::app);
-        myfile<<var<<"\n";
-        myfile.close();
     }
+
 
     return 0;
 //
