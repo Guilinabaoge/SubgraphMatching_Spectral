07b83fc233f6821004f8d6f4da691d0d35b8ca51
zhiwei <guilinabaoge@gmail.com>
2023-08-26 12:23:42 +0000

    experiments

--- test/tests/make_queries.sh
+++ test/tests/make_queries.sh
@@ -6,10 +6,8 @@ datasets=("dblp" "eu2005" "hprd" "patents" "yeast" "youtube" "wordnet" "human")
 
 for dataset in "${datasets[@]}"
   do
-    for query_number in $(seq 31 200)
+    for query_number in $(seq 1 50)
       do
-        python3 query_generator.py -s 64 -p dense -d "$dataset"
-        wait
-        python3 generate_wildcard_query.py -s 64 -prop dense -d "$dataset" -p 0.75 -n "$query_number"
+        python3 generate_wildcard_query.py -s 64 -prop dense -d "$dataset" -p 0.5 -n "$query_number"
       done
   done
