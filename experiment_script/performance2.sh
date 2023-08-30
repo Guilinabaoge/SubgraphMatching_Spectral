b0700ae8edb2eee71738dc0a622b2aefb4c6f357
Guilinabaoge <guilinabaoge@gmail.com>
2023-08-30 23:06:56 +0000

    experiments

--- experiment_script/performance2.sh
+++ experiment_script/performance2.sh
@@ -2,7 +2,8 @@
 #prop=("dense" "sparse")
 prop=("dense")
 #datasets=("dblp" "eu2005" "hprd" "patents" "yeast" "youtube" "wordnet" "human")
-datasets=("dblp" "eu2005" "hprd" "yeast" "wordnet" "human")
+#datasets=("dblp" "eu2005" "hprd" "yeast" "wordnet" "human")
+datasets=("patents" "youtube")
 for dataset in "${datasets[@]}"
 do
   for p in "${prop[@]}"
